e90d2e17f4a4a30e919ad65d5bfd096e2769691f
KhadijaMomar <souarekhadija12@gmail.com>
2025-01-23 17:42:50 +0100

    script api github

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
+
+
+
 window.onscroll = () =>{
     sections.forEach(sec => {
         let top = window.scrollY;
@@ -22,3 +25,35 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
+
+
+let logo = document.getElementById('logo');
+let loginElement = document.getElementById('login');
+let ville = document.getElementById('ville');
+const portfolioLink = document.getElementById('portfolio-link');
+
+fetch('https://api.github.com/users/KhadijaMomar')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('La requête a échoué');
+        }
+        return response.json(); // Convertit la réponse en JSON
+    })
+    .then(data => {
+
+        const portfolioUrl = data.html_url;
+
+        portfolioLink.href = portfolioUrl;
+
+        const login = data.login;
+        loginElement.textContent = `Login: ${login}`;
+
+        logo.src = data.avatar_url;
+
+    })
+    .catch(error => {
+        console.error('Erreur lors de la récupération de l\'URL:', error);
+
+        // Optionnel : Mettez un lien de secours en cas d'erreur
+        portfolioLink.href = "https://lien-de-secours.com";
+    });
